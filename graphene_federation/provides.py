0aa55fcceaadf447cbd7be19d779f98d4c3f217c
Arun Suresh Kumar <asuresh960@gmail.com>
2022-10-06 19:25:58 -0700

    Minor fix in @provides

--- graphene_federation/provides.py
+++ graphene_federation/provides.py
@@ -37,3 +37,20 @@ def provides(field, fields: Union[str, list[str]] = None):
             fields = fields.split()
         field._provides = fields
     return field
+
+
+def get_provides_fields(schema: Schema) -> []:
+    """
+    Find all the extended types from the schema.
+    They can be easily distinguished from the other type as
+    the `@provides` decorator adds a `_provides` attribute to them.
+    """
+    provides_fields = {}
+    for type_name, type_ in schema.graphql_schema.type_map.items():
+        if not hasattr(type_, "graphene_type"):
+            continue
+        for field in list(type_.graphene_type.__dict__):
+            if getattr(getattr(type_.graphene_type, field), "_provides", False):
+                provides_fields[type_name] = type_.graphene_type
+                continue
+    return provides_fields
