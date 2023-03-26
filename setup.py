14881c15fd22dcb14f9ade9339319dedcddeb07d
Erik Wrede <erikwrede@users.noreply.github.com>
2023-03-26 11:50:53 +0200

    release: 3.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 def read(*rnames):
   return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.1.0'
+version = '3.1.1'
 
 tests_require = [
     "pytest==7.1.2",
