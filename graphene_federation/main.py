24071dcd83b569959762931a9398c28a759ae186
Arun Suresh Kumar <asuresh960@gmail.com>
2023-12-10 18:20:43 +0530

    Bug Fix: Avoid schema rebuilding if provided as argument

--- graphene_federation/main.py
+++ graphene_federation/main.py
@@ -21,12 +21,13 @@ def _get_query(schema: Schema, query_cls: Optional[ObjectType] = None) -> Object
 
 
 def build_schema(
-    query: Optional[ObjectType] = None,
-    mutation: Optional[ObjectType] = None,
-    enable_federation_2=False,
-    **kwargs
+        query: Optional[ObjectType] = None,
+        mutation: Optional[ObjectType] = None,
+        enable_federation_2=False,
+        schema: Schema | None = None,
+        **kwargs
 ) -> Schema:
-    schema = Schema(query=query, mutation=mutation, **kwargs)
+    schema = schema or Schema(query=query, mutation=mutation, **kwargs)
     schema.auto_camelcase = kwargs.get("auto_camelcase", True)
     schema.federation_version = 2 if enable_federation_2 else 1
     federation_query = _get_query(schema, query)
