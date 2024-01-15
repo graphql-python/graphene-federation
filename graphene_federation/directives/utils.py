feb2a592090514923bf198603d3aab552e6ba176
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2024-01-16 00:23:57 +0530

    refact: is_non_field check

--- graphene_federation/directives/utils.py
+++ graphene_federation/directives/utils.py
@@ -1,20 +1,6 @@
+import inspect
 from typing import Any
-
-from graphene import Enum, InputObjectType, Interface, ObjectType, Scalar, Union
 
 
 def is_non_field(field: Any):
-    try:
-        if (
-            issubclass(field, ObjectType)
-            or issubclass(field, Interface)
-            or issubclass(field, InputObjectType)
-            or issubclass(field, Enum)
-            or issubclass(field, Union)
-            or issubclass(field, Scalar)
-        ):
-            return True
-        else:
-            return False
-    except TypeError:
-        return False
+    return inspect.isclass(field)
